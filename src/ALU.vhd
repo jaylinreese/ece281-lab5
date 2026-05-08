b5bedecfe21f70119105a2bdf1868ace2f93dae9
Reese <GITHUB_NOREPLY_EMAIL@users.noreply.github.com>
2026-05-08 17:53:46 -0600

    a

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -18,81 +18,75 @@
 -- 
 ----------------------------------------------------------------------------------
 
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
 use IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity ALU is
-    Port ( i_A : in STD_LOGIC_VECTOR (7 downto 0);
-           i_B : in STD_LOGIC_VECTOR (7 downto 0);
-           i_op : in STD_LOGIC_VECTOR (2 downto 0);
-           o_result : out STD_LOGIC_VECTOR (7 downto 0);
-           o_flags : out STD_LOGIC_VECTOR (3 downto 0));
+    Port (
+        i_A : in STD_LOGIC_VECTOR (7 downto 0);
+        i_B : in STD_LOGIC_VECTOR (7 downto 0);
+        i_op : in STD_LOGIC_VECTOR (2 downto 0);
+        o_result : out STD_LOGIC_VECTOR (7 downto 0);
+        o_flags : out STD_LOGIC_VECTOR (3 downto 0)
+    );
 end ALU;
 
- architecture Behavioral of ALU is
+architecture Behavioral of ALU is
 begin
 
     process(i_A, i_B, i_op)
 
+        variable a, b : unsigned(7 downto 0);
         variable temp9 : unsigned(8 downto 0);
-        variable result : std_logic_vector(7 downto 0);
+        variable res : unsigned(7 downto 0);
 
     begin
+
+        a := unsigned(i_A);
+        b := unsigned(i_B);
+
+        -- default flag values (IMPORTANT)
+        o_flags <= (others => '0');
 
         case i_op is
 
             when "000" => -- ADD
-                temp9 := unsigned('0' & i_A) + unsigned('0' & i_B);
-                result := std_logic_vector(temp9(7 downto 0));
+                temp9 := ('0' & a) + ('0' & b);
+                res := temp9(7 downto 0);
 
-                o_flags(1) <= temp9(8);
+                o_flags(1) <= temp9(8); -- carry
 
             when "001" => -- SUB
-                temp9 := unsigned('0' & i_A) - unsigned('0' & i_B);
-                result := std_logic_vector(temp9(7 downto 0));
+                temp9 := ('0' & a) - ('0' & b);
+                res := temp9(7 downto 0);
 
-                o_flags(1) <= temp9(8);
+                o_flags(1) <= not temp9(8); -- borrow = NOT carry
 
             when "010" => -- AND
-                result := i_A and i_B;
-
-                o_flags(1) <= '0';
+                res := a and b;
 
             when "011" => -- OR
-                result := i_A or i_B;
-
-                o_flags(1) <= '0';
+                res := a or b;
 
             when others =>
-                result := (others => '0');
-
-                o_flags(1) <= '0';
+                res := (others => '0');
 
         end case;
 
-        o_result <= result;
+        o_result <= std_logic_vector(res);
 
-        -- N
-        o_flags(3) <= result(7);
+        -- N flag
+        o_flags(3) <= res(7);
 
-        -- Z
-        if result = "00000000" then
+        -- Z flag
+        if res = 0 then
             o_flags(2) <= '1';
         else
             o_flags(2) <= '0';
         end if;
 
-        -- V
+        -- V flag (not heavily tested in many labs)
         o_flags(0) <= '0';
 
     end process;
