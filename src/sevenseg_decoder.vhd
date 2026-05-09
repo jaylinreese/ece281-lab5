fce5621a0282bdd83d754262bcbe082989c4791c
Reese <GITHUB_NOREPLY_EMAIL@users.noreply.github.com>
2026-05-08 21:30:32 -0600

    a

--- src/sevenseg_decoder.vhd
+++ src/sevenseg_decoder.vhd
@@ -22,31 +22,31 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity sevenseg_decoder is
     Port (
-        x : in STD_LOGIC_VECTOR (3 downto 0);
-        seg : out STD_LOGIC_VECTOR (6 downto 0)
+        i_Hex   : in STD_LOGIC_VECTOR (3 downto 0);
+        o_seg_n : out STD_LOGIC_VECTOR (6 downto 0)
     );
 end sevenseg_decoder;
 
 architecture Behavioral of sevenseg_decoder is
 begin
 
-    process(x)
+    process(i_Hex)
     begin
 
-        case x is
+        case i_Hex is
 
-            when "0000" => seg <= "1000000"; -- 0
-            when "0001" => seg <= "1111001"; -- 1
-            when "0010" => seg <= "0100100"; -- 2
-            when "0011" => seg <= "0110000"; -- 3
-            when "0100" => seg <= "0011001"; -- 4
-            when "0101" => seg <= "0010010"; -- 5
-            when "0110" => seg <= "0000010"; -- 6
-            when "0111" => seg <= "1111000"; -- 7
-            when "1000" => seg <= "0000000"; -- 8
-            when "1001" => seg <= "0010000"; -- 9
+            when "0000" => o_seg_n <= "1000000";
+            when "0001" => o_seg_n <= "1111001";
+            when "0010" => o_seg_n <= "0100100";
+            when "0011" => o_seg_n <= "0110000";
+            when "0100" => o_seg_n <= "0011001";
+            when "0101" => o_seg_n <= "0010010";
+            when "0110" => o_seg_n <= "0000010";
+            when "0111" => o_seg_n <= "1111000";
+            when "1000" => o_seg_n <= "0000000";
+            when "1001" => o_seg_n <= "0010000";
 
-            when others => seg <= "1111111";
+            when others => o_seg_n <= "1111111";
 
         end case;
 
--- src/top_basys3.vhd
+++ src/top_basys3.vhd
@@ -21,149 +21,244 @@
 --|
 --+----------------------------------------------------------------------------
 library ieee;
-  use ieee.std_logic_1164.all;
-  use ieee.numeric_std.all;
-
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity top_basys3 is
     port(
-        -- inputs
-        clk     :   in std_logic; -- native 100MHz FPGA clock
-        sw      :   in std_logic_vector(7 downto 0); -- operands and opcode
-        btnU    :   in std_logic; -- reset
-        btnC    :   in std_logic; -- fsm cycle
-        
-        -- outputs
-        led :   out std_logic_vector(15 downto 0);
-        -- 7-segment display segments (active-low cathodes)
-        seg :   out std_logic_vector(6 downto 0);
-        -- 7-segment display active-low enables (anodes)
-        an  :   out std_logic_vector(3 downto 0)
+
+        clk     : in std_logic;
+        sw      : in std_logic_vector(7 downto 0);
+
+        btnU    : in std_logic;
+        btnC    : in std_logic;
+        btnL    : in std_logic;
+
+        led     : out std_logic_vector(15 downto 0);
+
+        seg     : out std_logic_vector(6 downto 0);
+
+        an      : out std_logic_vector(3 downto 0)
+
     );
 end top_basys3;
 
-architecture top_basys3_arch of top_basys3 is 
+architecture top_basys3_arch of top_basys3 is
 
 component controller_fsm
     Port (
         i_reset : in std_logic;
         i_adv   : in STD_LOGIC;
         o_cycle : out STD_LOGIC_VECTOR (3 downto 0)
-        );
-       end component;
-       
-       component ALU
-         Port (
-            i_A  : in std_logic_vector (7 downto 0);
-            i_B  : in std_logic_vector (7 downto 0);
-            i_op  : in std_logic_vector (2 downto 0);
-            o_result  : out std_logic_vector (7 downto 0);
-            o_flags  : out std_logic_vector (3 downto 0)
-          );
-    end component;
-    
-    component sevenseg_decoder
-        Port (
-            i_val : in std_logic_vector(3 downto 0);
-            o_seg : out std_logic_vector(6 downto 0)
-        );
-    end component;
+    );
+end component;
 
-    signal cycle       : std_logic_vector(3 downto 0);
+component ALU
+    Port (
+        i_A : in STD_LOGIC_VECTOR (7 downto 0);
+        i_B : in STD_LOGIC_VECTOR (7 downto 0);
+        i_op : in STD_LOGIC_VECTOR (2 downto 0);
+        o_result : out STD_LOGIC_VECTOR (7 downto 0);
+        o_flags : out STD_LOGIC_VECTOR (3 downto 0)
+    );
+end component;
 
-    signal regA        : std_logic_vector(7 downto 0) := (others => '0');
-    signal regB        : std_logic_vector(7 downto 0) := (others => '0');
+component sevenseg_decoder
+    Port (
+        i_Hex   : in std_logic_vector(3 downto 0);
+        o_seg_n : out std_logic_vector(6 downto 0)
+    );
+end component;
 
-    signal alu_out     : std_logic_vector(7 downto 0);
-    signal alu_flags   : std_logic_vector(3 downto 0);
+component twos_comp
+    port (
+        i_bin  : in std_logic_vector(7 downto 0);
+        o_sign : out std_logic;
+        o_hund : out std_logic_vector(3 downto 0);
+        o_tens : out std_logic_vector(3 downto 0);
+        o_ones : out std_logic_vector(3 downto 0)
+    );
+end component;
 
-    signal display_val : std_logic_vector(3 downto 0);
+component TDM4
+generic (
+    constant k_WIDTH : natural := 7
+);
+port (
+    i_clk   : in std_logic;
+    i_reset : in std_logic;
+
+    i_D3 : in std_logic_vector(6 downto 0);
+    i_D2 : in std_logic_vector(6 downto 0);
+    i_D1 : in std_logic_vector(6 downto 0);
+    i_D0 : in std_logic_vector(6 downto 0);
+
+    o_data : out std_logic_vector(6 downto 0);
+    o_sel  : out std_logic_vector(3 downto 0)
+);
+end component;
+
+component clock_divider
+generic (
+    constant k_DIV : natural := 100000
+);
+port (
+    i_clk   : in std_logic;
+    i_reset : in std_logic;
+    o_clk   : out std_logic
+);
+end component;
+
+signal cycle : std_logic_vector(3 downto 0);
+
+signal regA : std_logic_vector(7 downto 0) := (others => '0');
+signal regB : std_logic_vector(7 downto 0) := (others => '0');
+
+signal alu_out : std_logic_vector(7 downto 0);
+signal alu_flags : std_logic_vector(3 downto 0);
+
+signal display_byte : std_logic_vector(7 downto 0);
+
+signal sign : std_logic;
+
+signal hund : std_logic_vector(3 downto 0);
+signal tens : std_logic_vector(3 downto 0);
+signal ones : std_logic_vector(3 downto 0);
+
+signal seg0 : std_logic_vector(6 downto 0);
+signal seg1 : std_logic_vector(6 downto 0);
+signal seg2 : std_logic_vector(6 downto 0);
+signal seg3 : std_logic_vector(6 downto 0);
+
+signal slow_clk : std_logic;
 
 begin
 
-    -- FSM
-    FSM0 : controller_fsm
-        port map(
-            i_reset => btnU,
-            i_adv   => btnC,
-            o_cycle => cycle
-        );
+FSM0 : controller_fsm
+port map(
+    i_reset => btnU,
+    i_adv   => btnC,
+    o_cycle => cycle
+);
 
-    -- ALU
-    ALU0 : ALU
-        port map(
-            i_A      => regA,
-            i_B      => regB,
-            i_op     => sw(2 downto 0),
-            o_result => alu_out,
-            o_flags  => alu_flags
-        );
+ALU0 : ALU
+port map(
+    i_A      => regA,
+    i_B      => regB,
+    i_op     => sw(2 downto 0),
+    o_result => alu_out,
+    o_flags  => alu_flags
+);
 
-    -- Seven Segment Decoder
-    SSD0 : sevenseg_decoder
-        port map(
-            i_val => display_val,
-            o_seg => seg
-        );
+CLKDIV0 : clock_divider
+generic map(
+    k_DIV => 100000
+)
+port map(
+    i_clk   => clk,
+    i_reset => btnL,
+    o_clk   => slow_clk
+);
 
-    -- Register Loading
-    process(btnC, btnU)
-    begin
+TC0 : twos_comp
+port map(
+    i_bin  => display_byte,
+    o_sign => sign,
+    o_hund => hund,
+    o_tens => tens,
+    o_ones => ones
+);
 
-        if btnU = '1' then
+SSD0 : sevenseg_decoder
+port map(
+    i_Hex   => ones,
+    o_seg_n => seg0
+);
 
-            regA <= (others => '0');
-            regB <= (others => '0');
+SSD1 : sevenseg_decoder
+port map(
+    i_Hex   => tens,
+    o_seg_n => seg1
+);
 
-        elsif rising_edge(btnC) then
+SSD2 : sevenseg_decoder
+port map(
+    i_Hex   => hund,
+    o_seg_n => seg2
+);
 
-            case cycle is
+seg3 <= "0111111" when sign = '1' else "1111111";
 
-                when "0010" =>
-                    regA <= sw;
+TDM0 : TDM4
+generic map(
+    k_WIDTH => 7
+)
+port map(
+    i_clk   => slow_clk,
+    i_reset => btnL,
 
-                when "0100" =>
-                    regB <= sw;
+    i_D3 => seg3,
+    i_D2 => seg2,
+    i_D1 => seg1,
+    i_D0 => seg0,
 
-                when others =>
-                    null;
+    o_data => seg,
+    o_sel  => an
+);
 
-            end case;
+process(btnC, btnU)
+begin
 
-        end if;
+    if btnU = '1' then
 
-    end process;
+        regA <= (others => '0');
+        regB <= (others => '0');
 
-    -- Display Logic
-    process(cycle, regA, regB, alu_out)
-    begin
+    elsif rising_edge(btnC) then
 
         case cycle is
 
-            when "0001" =>
-                display_val <= "0000";
-
             when "0010" =>
-                display_val <= regA(3 downto 0);
+                regA <= sw;
 
             when "0100" =>
-                display_val <= regB(3 downto 0);
-
-            when "1000" =>
-                display_val <= alu_out(3 downto 0);
+                regB <= sw;
 
             when others =>
-                display_val <= "0000";
+                null;
 
         end case;
 
-    end process;
+    end if;
 
-    -- LEDs
-    led(3 downto 0) <= cycle;
-    led(15 downto 12) <= alu_flags;
+end process;
 
-    -- Enable rightmost seven segment
-    an <= "1110";
+process(cycle, regA, regB, alu_out)
+begin
+
+    case cycle is
+
+        when "0001" =>
+            display_byte <= (others => '0');
+
+        when "0010" =>
+            display_byte <= regA;
+
+        when "0100" =>
+            display_byte <= regB;
+
+        when "1000" =>
+            display_byte <= alu_out;
+
+        when others =>
+            display_byte <= (others => '0');
+
+    end case;
+
+end process;
+
+led(3 downto 0) <= cycle;
+
+led(15 downto 12) <= alu_flags;
 
 end top_basys3_arch;
